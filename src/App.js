403b235a78805cd7f2f615e6d72f0dc2e5b29b8c
unizazang <zifnffk32123@gmail.com>
2023-01-18 17:13:09 +0900

    20230118

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react"; //react 에서 컴포넌트를 쓸수있도록 가져옴
+/* 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Hello React!</h1>
     </div>
   );
+} 
+
+*/
+// 함수로 컴포넌트 만들었음. + 클래스로 만들수도 있어요 >> rcc.
+
+//App > 여기서
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* 주의사항 : 이 div 안에 모든 태그가 있어야해요. - 이거 주석 이렇게생긴게맞음 어차피 화면에안나와~~ */}
+        <div className="App">
+          <h1 className="title">Hello React!</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+// export default App;
+//그대로 모듈형식으로 가져오기 위해 export default
+// export 해서 추출된 모듈을 (컴포넌트라고도 부름) > 을
+/* 
+  <React.StrictMode>
+    <App /> <<<<< 이렇게 씀 <App></App>
+  </React.StrictMode>
+   */
